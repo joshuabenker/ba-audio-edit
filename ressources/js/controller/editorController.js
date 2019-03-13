75b30fa1e5f91736444b61fb84d04d8d9a7273b8
joshuabenker <joshua@benker.info>
2019-03-13 18:23:09 +0100

    mvc-modell update

--- ressources/js/controller/editorController.js
+++ ressources/js/controller/editorController.js
@@ -3,14 +3,12 @@ audioEditor.editorController = function() {
   "use strict";
   var that = {};
 
-  function init(){}
 
   function test(){
     console.log("editorcontroller: yes.");
   }
 
   that.test=test;
-  that.init = init;
   return that;
 
 };
